a2765762a0e3d04ed2b1c980c6ae3da9294f4a5b
Timkajke <105926655+Timkajke@users.noreply.github.com>
2022-10-24 16:54:04 +0700

    hw4 1/2 is done
    hw4 1/2 is done

--- scripts.js
+++ scripts.js
@@ -51,3 +51,59 @@ if (number > 0) {
 else {
     console.log("Oops! Something went wrong! Try again!");
 } */
+
+// Определить количество цифр в введенном числе.
+
+/* var number = +prompt("Enter number to find out how many digits is has");
+var quantityOfDigits = 0;
+if (number === 0) {
+    quantityOfDigits = 1;
+    console.log(`You number has ${quantityOfDigits} digits`);
+}
+else {
+    for (quantityOfDigits; number >= 1; quantityOfDigits++) {
+        number /= 10;
+    }
+    console.log(`You number has ${quantityOfDigits} digits`);
+} */
+
+// Запросить у пользователя 10 чисел и подсчитать, сколько он ввел положительных, отрицательных и нулей. 
+// При этом также посчитать, сколько четных и нечетных. Вывести статистику на экран. 
+// Учтите, что достаточно одной переменной (не 10) для ввода чисел пользователем.
+
+/* НЕ РЕШЕНО */
+
+// Зациклить калькулятор. Запросить у пользователя 2 числа и знак, решить пример, 
+// вывести результат и спросить, хочет ли он решить еще один пример. 
+// И так до тех пор, пока пользователь не откажется
+
+/* do {
+    var firstNumber = +prompt("Enter first number");
+    var secondNumber = +prompt("Enter second number");
+    var operand = prompt("Enter operand");
+    switch (operand) {
+        case "+":
+            alert(`Your result is: ${firstNumber + secondNumber}`);
+            break;
+        case "-":
+            alert(`Your result is: ${firstNumber - secondNumber}`);
+            break;
+        case "*":
+            alert(`Your result is: ${firstNumber * secondNumber}`);
+            break;
+        case "/":
+            if (secondNumber > 0) {
+                alert(`Your result is: ${firstNumber / secondNumber}`);
+                break;
+            }
+            else {
+                alert("Wrong operation! Try again!");
+                break;
+            }
+        default:
+            alert("Wrong operation! Try again!");
+            break;
+    }
+}
+while (confirm("Do you want to try again?")); */
+
